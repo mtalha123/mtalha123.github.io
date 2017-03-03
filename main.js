28e68902f9e50e592376722ff71e0f3f6419ec86
Muhammad Talha <muhtalha132@hotmail.com>
2017-03-03 13:06:25 -0800

    Update main.js

--- main.js
+++ main.js
@@ -103,13 +103,13 @@ var UWImage = new Image();
 UWImage.src = "uwtexture.jpg";
 
 UWImage.onload = function(){
+ gl.activeTexture(gl.TEXTURE1);
     gl.bindTexture(gl.TEXTURE_2D, UWTexture);
     gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
     gl.texImage2D(gl.TEXTURE_2D, 1, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, UWImage);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
 };
-
 gl.activeTexture(gl.TEXTURE1);
 gl.bindTexture(gl.TEXTURE_2D, UWTexture);
 
